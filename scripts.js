e12a8b27815877a27dc4062695b28b25f96166ef
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2021-10-31 13:09:46 +0300

    feat: add slider

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-
+$('.code-examples').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  variableWidth: true,
+  centerMode: true,
+  centerPadding: '0',
+  prevArrow: $('.slider__prev'),
+  nextArrow: $('.slider__next'),
+  dots: true, // показывать точки индикаторы
+  dotsClass: 'slick-dots', // название класса для точек
+  responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 510,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
